52b342ac6d32e85f42f8d66fa497e932b68af6f8
GauriVenkat <31267185+GauriVenkat@users.noreply.github.com>
2024-10-14 14:27:14 +0000

    Initial functions added and passing tests

--- src/hypot/pythag.py
+++ src/hypot/pythag.py
@@ -35,9 +35,38 @@ def square_number(a):
     return squared
 # Function to calculate the square root of a number
 # input: floats or int or list/array of ints/floats
+def square_root(a):
+    """Calculate square root 
+
+    Args:
+        a (float, int, array): Number whose square root is needed
+
+    Returns:
+        float, int, array: Square root of a
+    """
+    sqrt_num = a ** 0.5
+    return sqrt_num
+
 
 # Function to calculate the hypotenuse using all the above
 # a^2 + b^2 = c^2
 # c = sqrt(a^2 + b^2)
 # input: floats or int or list/array of ints/floats
 
+def calc_hypot(opp, adj):
+    """Calculate the length of the hypotenuse of a Right angled triangle
+
+    Args:
+        opp (float, int, array): Length of opposite side
+        adj (float, int, array): Length of adjacent side
+
+    Returns:
+        float, int, array: Length of hypotenuse
+    """
+    #opposite and adjacent
+    opp_sqrd = square_number(opp)
+    adj_sqrd = square_number(adj)
+    summed_nums = add_nums(opp_sqrd, adj_sqrd)
+    hypotenuse = square_root(summed_nums)
+    return hypotenuse
+    
--- tests/test_pythag.py
+++ tests/test_pythag.py
@@ -56,3 +56,70 @@ def test_square_array():
 
     # Assert
     assert np.allclose(expected_output, output)
+    
+def test_square_root():
+    '''Tests for the square root function'''
+
+    # Arrange
+    test_variable_1 = 16
+    expected_output = 4
+
+    # Act
+    output = hypot.pythag.square_root(test_variable_1)
+
+    # Assert
+    assert output == expected_output
+    
+def test_square_root_array():
+    '''Tests for the square root function for an array'''
+
+    # Arrange
+    test_array_1 = np.array([16, 9, 64])
+    expected_output = np.array([4, 3, 8])
+
+    # Act
+    output = hypot.pythag.square_root(test_array_1)
+
+    # Assert
+    assert np.allclose(expected_output, output)
+    
+def test_square_root_2():
+    '''Tests for the square root function'''
+
+    # Arrange
+    test_variable_1 = 16
+    expected_output = np.sqrt(16)
+
+    # Act
+    output = hypot.pythag.square_root(test_variable_1)
+
+    # Assert
+    assert output == expected_output
+    
+def test_calc_hypot():
+    """Integration for hypot
+    """
+    # Arrange
+    test_variable_1 = 5.6
+    test_variable_2 = 7.8
+    expected_output = 9.60208
+
+    # Act
+    output = hypot.pythag.calc_hypot(test_variable_1, test_variable_2)
+
+    # Assert
+    assert np.allclose(expected_output, output, atol = 1e-3)
+    
+def test_calc_hypot():
+    """Integration for hypot
+    """
+    # Arrange
+    test_variable_1 = np.array([10, 30045, 6.9, 0.04])
+    test_variable_2 = np.array([23, 2400, 8.9, 0.34])
+    expected_output = np.array([25.08, 30140.7, 11.26, 0.34])
+
+    # Act
+    output = hypot.pythag.calc_hypot(test_variable_1, test_variable_2)
+
+    # Assert
+    assert np.allclose(expected_output, output, rtol= 1e-1)
